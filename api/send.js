d8baab3fc949f70e3c2a6193b083fefac3cd3fa9
Samuel <snelo1390@gmail.com>
2026-01-16 09:10:06 -0500

    debug

--- api/send.js
+++ api/send.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
 
   try {
     const data = await resend.emails.send({
-      from: `${req.body.email || "<onboarding@resend.dev>"}`,
+      from: `${req.body.email}`,
       to: ["snelo1390@gmail.com"],
       subject: "Nuevo contacto del Portfolio",
       html: `<h2>Mensaje: ${req.body.message}</h2>`,
