f020ac0ee0470bc225f2db6cde6a5382218ba119
Samuel <snelo1390@gmail.com>
2026-01-16 09:24:37 -0500

    debug

--- api/send.js
+++ api/send.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
 
   try {
     const data = await resend.emails.send({
-      from: `${req.body.email}`,
+      from: `${req.body.email}@https://samuel-nelo-portfolio-nsch9ko05-samuel-nelos-projects.vercel.app`,
       to: ["snelo1390@gmail.com"],
       subject: "Nuevo contacto del Portfolio",
       html: `<h2>Mensaje: ${req.body.message}</h2>`,
